6ef623f1234bbe41e9d85cf7448072333e90c61c
Wadie Skaf <wadie.skaf@live.com>
2019-10-15 15:31:12 +0200

    added model dependent ordinal strategies (minimum available choices block to be chosen first)

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-with open('sudoku_Regions.txt') as f:
+with open('Sudoku_Regions.txt') as f:
     content = f.read()
 dict_construct = 'regions_dict = {' + content + '}'
 regions_dict = {}
@@ -9,16 +9,24 @@ numbers_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 
 def construct_sudoku_array():
-    with open('sudoku.txt') as f:
+    with open('Sudoku.txt') as f:
         file_content = list(filter((lambda x: x != ',' and x != '\n'), list(f.read())))
     sudoku_array = np.array(file_content, dtype=int).reshape(9, 9)
     return sudoku_array
 
 
 def find_empty(x):
+    empty_items_list = []
     for index, item in np.ndenumerate(x):
         if item == 0:
-            return (index)
+            empty_items_list.append(index)
+    if len(empty_items_list) != 0:
+        empty_items_num_of_available_numbers = []
+        for i in empty_items_list:
+            a = len(find_available_numbers(x, i[0], i[1]))
+            empty_items_num_of_available_numbers.append(a)
+
+        return empty_items_list[empty_items_num_of_available_numbers.index(min(empty_items_num_of_available_numbers))]
     return None
 
 
@@ -57,5 +65,7 @@ def solve(sudoku_array):
 
 
 sudoku = construct_sudoku_array()
+
+print(find_empty(sudoku))
 solve(sudoku)
 print(sudoku)
