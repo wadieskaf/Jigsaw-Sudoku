0fdbff6c410b9ff914a59ad5b2003fb22c9b21f0
Wadie Skaf <wadie.skaf@live.com>
2019-11-06 11:53:23 +0100

    Delete some files

--- Test2.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-with open('Sudoku.txt') as f:
-    content = list(filter((lambda x: x != ',' and x != '\n'), list(f.read())))
-sudoko_matrix = np.array(content, dtype=int).reshape(9, 9)
-
-print(sudoko_matrix[(0,1)])
